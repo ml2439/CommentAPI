db44f182d071d31df6ecdc3e4b5a1b1fbdbaa2e0
ml2439 <ml2439@cornell.edu>
2017-07-15 15:56:32 -0400

    Initial commit

---
+++ CommentAPI/Controllers/CommentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CommentAPI.Controllers
+{
+    [Route("api/comment")]
+    public class CommentController : Controller
+    {
+        public JsonResult GetComments()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id=1, Content="this is a comment." },
+                new { id=2, Content="this is another comment." }
+            });
+        }
+    }
+}
